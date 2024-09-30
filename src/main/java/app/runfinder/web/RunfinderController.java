2f5841122a393cc74b77bf28244ca0693e7037bf
devaajanne <devaajanne@gmail.com>
2024-09-30 13:40:09 +0300

    Added RunningGroupRepository to controller

--- src/main/java/app/runfinder/web/RunfinderController.java
+++ src/main/java/app/runfinder/web/RunfinderController.java
@@ -1,15 +1,22 @@
 package app.runfinder.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import app.runfinder.domain.RunningGroupRepository;
+
 import org.springframework.ui.Model;
 
 @Controller
 public class RunfinderController {
 
+    @Autowired
+    private RunningGroupRepository runningGroupRepository;
+
     @GetMapping("/runninggroups")
     public String showRunningGroups(Model model) {
-        model.addAttribute("runningGroups", runningGroupRepository.findall());
+        model.addAttribute("runningGroups", runningGroupRepository.findAll());
         return "runningGroups";
     }
 }
