5267209a22e1f3ad9528e88bc66d4f8a34c42d8f
devaajanne <devaajanne@gmail.com>
2024-09-30 12:51:36 +0300

    Added Role entity class

---
+++ src/main/java/app/runfinder/domain/Role.java
@@ -0,0 +1,62 @@
+package app.runfinder.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Role id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Role role(String role) {
+        setRole(role);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", role='" + getRole() + "'" +
+                "}";
+    }
+}
