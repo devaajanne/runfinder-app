15d82407fe55483510c344fa1a30d0a8e216483d
devaajanne <devaajanne@gmail.com>
2024-09-30 14:19:04 +0300

    Added CommandLineRunner to application

--- src/main/java/app/runfinder/RunfinderApplication.java
+++ src/main/java/app/runfinder/RunfinderApplication.java
@@ -1,7 +1,14 @@
 package app.runfinder;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import app.runfinder.domain.RunningGroup;
+import app.runfinder.domain.RunningGroupRepository;
+import app.runfinder.domain.Zipcode;
+import app.runfinder.domain.ZipcodeRepository;
 
 @SpringBootApplication
 public class RunfinderApplication {
@@ -9,5 +16,12 @@ public class RunfinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RunfinderApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner runfinderCLR(RunningGroupRepository runningGroupRepository,
+			ZipcodeRepository zipcodeRepository) {
+		return (args) -> {
+		};
+	};
 
 }
