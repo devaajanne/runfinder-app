20cc1314d96c667b7c87e4cd32f95fa6765f0402
devaajanne <devaajanne@gmail.com>
2024-09-30 17:49:03 +0300

    Added default REST API mapping to '/api'

--- src/main/java/app/runfinder/web/RestRunfinderController.java
+++ src/main/java/app/runfinder/web/RestRunfinderController.java
@@ -7,8 +7,10 @@ import app.runfinder.domain.RunningGroupRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class RestRunfinderController {
 
     @Autowired
