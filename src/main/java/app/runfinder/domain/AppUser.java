e3966680db4ee5082023071e33752571f99213ca
devaajanne <devaajanne@gmail.com>
2024-09-30 12:47:26 +0300

    Added appUser entity class

---
+++ src/main/java/app/runfinder/domain/AppUser.java
@@ -0,0 +1,152 @@
+package app.runfinder.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "appUsers")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "password")
+    private String password;
+
+    public AppUser() {
+    }
+
+    public AppUser(String username, String firstName, String lastName, String email, Role role, String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AppUser id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public AppUser username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public AppUser firstName(String firstName) {
+        setFirstName(firstName);
+        return this;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public AppUser lastName(String lastName) {
+        setLastName(lastName);
+        return this;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AppUser email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public AppUser role(Role role) {
+        setRole(role);
+        return this;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AppUser password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", username='" + getUsername() + "'" +
+                ", firstName='" + getFirstName() + "'" +
+                ", lastName='" + getLastName() + "'" +
+                ", email='" + getEmail() + "'" +
+                ", role='" + getRole() + "'" +
+                ", password='" + getPassword() + "'" +
+                "}";
+    }
+}
