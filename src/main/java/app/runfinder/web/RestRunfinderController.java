795a95fd5c75dcb1e456b79e00dfd68d9165fca8
devaajanne <devaajanne@gmail.com>
2024-10-01 10:43:44 +0300

    Added PUT mapping to REST API controller

--- src/main/java/app/runfinder/web/RestRunfinderController.java
+++ src/main/java/app/runfinder/web/RestRunfinderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +39,13 @@ public class RestRunfinderController {
         return newRunningGroup;
     }
 
+    @PutMapping("runninggroups/{runningGroupId}")
+    public RunningGroup putRunningGroup(@RequestBody RunningGroup editedRunningGroup, @PathVariable Long runningGroupId) {
+        editedRunningGroup.setRunningGroupId(runningGroupId);
+        runningGroupRepository.save(editedRunningGroup);
+        return editedRunningGroup;
+    }
+    
     @DeleteMapping("runninggroups/{runningGroupId}")
     public Iterable<RunningGroup> deleteRunningGroup(@PathVariable Long runningGroupId) {
         runningGroupRepository.deleteById(runningGroupId);
