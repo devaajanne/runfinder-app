bda17509b03edbda2f4dab117b801d61d0a2f573
devaajanne <devaajanne@gmail.com>
2024-10-01 11:28:54 +0300

    Added REST controller for getting zipcodes

---
+++ src/main/java/app/runfinder/web/RestZipcodeController.java
@@ -0,0 +1,29 @@
+package app.runfinder.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.runfinder.domain.Zipcode;
+import app.runfinder.domain.ZipcodeRepository;
+
+@RestController
+@RequestMapping("/api")
+public class RestZipcodeController {
+
+    @Autowired
+    ZipcodeRepository zipcodeRepository;
+
+    @GetMapping("/zipcodes")
+    public Iterable<Zipcode> getZipcodes() {
+        return zipcodeRepository.findAll();
+    }
+
+    @GetMapping("/zipcodes/{zipcode}")
+    public Zipcode getZipcodes(@PathVariable String zipcode) {
+        return zipcodeRepository.findByZipcodeId(zipcode);
+    }
+
+}
