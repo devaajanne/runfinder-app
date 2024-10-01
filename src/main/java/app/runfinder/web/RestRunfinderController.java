2556fefc414648fe00fa54976796e9f7469702f2
devaajanne <devaajanne@gmail.com>
2024-10-01 09:56:05 +0300

    Added individual running group REST GET mapping

--- src/main/java/app/runfinder/web/RestRunfinderController.java
+++ src/main/java/app/runfinder/web/RestRunfinderController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import app.runfinder.domain.RunningGroup;
 import app.runfinder.domain.RunningGroupRepository;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -19,5 +22,10 @@ public class RestRunfinderController {
     @GetMapping("/runninggroups")
     public Iterable<RunningGroup> getRunningGroups() {
         return runningGroupRepository.findAll();
+    }
+
+    @GetMapping("runninggroups/{runningGroupId}")
+    public Optional<RunningGroup> getRunningGroup(@PathVariable Long runningGroupId) {
+        return runningGroupRepository.findById(runningGroupId);
     }
 }
