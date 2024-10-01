eea1e4c8c3502b2a2e8902fd9323b3e3f6bdba57
devaajanne <devaajanne@gmail.com>
2024-10-01 09:58:33 +0300

    Added DELETE mapping to REST API controller

--- src/main/java/app/runfinder/web/RestRunfinderController.java
+++ src/main/java/app/runfinder/web/RestRunfinderController.java
@@ -8,6 +8,7 @@ import app.runfinder.domain.RunningGroupRepository;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,11 @@ public class RestRunfinderController {
     @GetMapping("runninggroups/{runningGroupId}")
     public Optional<RunningGroup> getRunningGroup(@PathVariable Long runningGroupId) {
         return runningGroupRepository.findById(runningGroupId);
+    }
+
+    @DeleteMapping("runninggroups/{runningGroupId}")
+    public Iterable<RunningGroup> deleteRunningGroup(@PathVariable Long runningGroupId) {
+        runningGroupRepository.deleteById(runningGroupId);
+        return runningGroupRepository.findAll();
     }
 }
