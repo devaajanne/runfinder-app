ae38b8547ac6c43fcaf3c481721187b408e3728f
devaajanne <devaajanne@gmail.com>
2024-10-10 13:01:12 +0300

    Added REST controller to role entity class

---
+++ src/main/java/app/runfinder/web/RestRoleController.java
@@ -0,0 +1,31 @@
+package app.runfinder.web;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import app.runfinder.domain.Role;
+import app.runfinder.domain.RoleRepository;
+
+@RestController
+@RequestMapping("/api")
+public class RestRoleController {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @GetMapping("/roles")
+    public Iterable<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @GetMapping("/roles/{id}")
+    public Optional<Role> getRoleById(@PathVariable("id") Long id) {
+        return roleRepository.findById(id);
+    }
+
+}
