f2ae45891261f3e762d3e65aa89e56db49f2f97e
devaajanne <devaajanne@gmail.com>
2024-10-15 11:00:27 +0300

    Added query method findByUsername to AppUserRepository

--- src/main/java/app/runfinder/domain/repositories/AppUserRepository.java
+++ src/main/java/app/runfinder/domain/repositories/AppUserRepository.java
@@ -6,5 +6,5 @@ import app.runfinder.domain.entities.AppUser;
 
 // Provdes CRUD functions to the entity class
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
-    // No additional CRUD methods created
+    AppUser findByUsername(String username);
 }
