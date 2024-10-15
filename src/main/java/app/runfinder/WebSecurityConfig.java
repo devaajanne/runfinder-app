a73fa7fb42910c0664ca951e12d704b0b64fd11f
devaajanne <devaajanne@gmail.com>
2024-10-15 11:14:49 +0300

    Changed spring security config to user UserDetailService implementation

--- src/main/java/app/runfinder/WebSecurityConfig.java
+++ src/main/java/app/runfinder/WebSecurityConfig.java
@@ -1,12 +1,23 @@
 package app.runfinder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import app.runfinder.web.services.UserDetailServiceImpl;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private UserDetailServiceImpl userDetailServiceImpl;
+
+    public WebSecurityConfig(UserDetailServiceImpl userDetailServiceImpl) {
+        this.userDetailServiceImpl = userDetailServiceImpl;
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -23,5 +34,10 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
