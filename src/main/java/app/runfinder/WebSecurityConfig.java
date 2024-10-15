b75fabad5a0cf6a0715cf446f44ae92cd9fa6eed
devaajanne <devaajanne@gmail.com>
2024-10-15 13:37:04 +0300

    Fixed password encryption error

--- src/main/java/app/runfinder/WebSecurityConfig.java
+++ src/main/java/app/runfinder/WebSecurityConfig.java
@@ -15,17 +15,11 @@ import app.runfinder.web.services.UserDetailServiceImpl;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
-    private UserDetailServiceImpl userDetailServiceImpl;
-
-    public WebSecurityConfig(UserDetailServiceImpl userDetailServiceImpl) {
-        this.userDetailServiceImpl = userDetailServiceImpl;
-    }
-
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/signup", "/rungrouplist").permitAll()
+                        .requestMatchers("/login", "/signup", "/savenewuser").permitAll()
                         .requestMatchers("/add", "/delete/**", "/edit/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(formlogin -> formlogin.loginPage("/login")
@@ -38,8 +32,8 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
