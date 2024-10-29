a33aaaec05be1e86393f21d264b6b4000e48e565
devaajanne <devaajanne@gmail.com>
2024-10-29 16:00:07 +0200

    Separated RunGroupDTO to GET and POST/PUT versions

--- src/main/java/app/runfinder/domain/dto/RunGroupGetDTO.java
+++ src/main/java/app/runfinder/domain/dto/RunGroupGetDTO.java
@@ -3,7 +3,7 @@ package app.runfinder.domain.dto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record RunGroupDTO(
+public record RunGroupGetDTO(
     long runGroupId,
     String runGroupName,
     LocalDate runStartDate,
---
+++ src/main/java/app/runfinder/domain/dto/RunGroupPostPutDTO.java
@@ -0,0 +1,16 @@
+package app.runfinder.domain.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record RunGroupPostPutDTO(
+    @Size(min = 5, max = 50, message = "Run group name must be between 5 and 50 characters long") String runGroupName,
+    @NotNull(message = "Run group must have a start date") LocalDate runStartDate,
+    @NotNull(message = "Run group must have a start time") LocalTime runStartTime,
+    @NotEmpty(message = "Run group must have a start address") String startAddress,
+    @NotEmpty(message = "Run group must have a zipcode") String zipcode
+) {}
