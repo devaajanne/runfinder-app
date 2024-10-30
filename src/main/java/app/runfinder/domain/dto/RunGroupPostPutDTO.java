8a792289c3233e43930c782b141aac7c43f4726e
devaajanne <devaajanne@gmail.com>
2024-10-30 11:33:14 +0200

    Fixed validation rules

--- src/main/java/app/runfinder/domain/dto/RunGroupPostPutDTO.java
+++ src/main/java/app/runfinder/domain/dto/RunGroupPostPutDTO.java
@@ -8,9 +8,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record RunGroupPostPutDTO(
-    @Size(min = 5, max = 50, message = "Run group name must be between 5 and 50 characters long") String runGroupName,
-    @NotNull(message = "Run group must have a start date") LocalDate runStartDate,
-    @NotNull(message = "Run group must have a start time") LocalTime runStartTime,
-    @NotEmpty(message = "Run group must have a start address") String startAddress,
-    @NotEmpty(message = "Run group must have a zipcode") String zipcode
-) {}
+                @Size(min = 5, max = 50, message = "Run group name must be between 5 and 50 characters long") String runGroupName,
+                @NotNull(message = "Run group must have a start date") LocalDate runStartDate,
+                @NotNull(message = "Run group must have a start time") LocalTime runStartTime,
+                @NotEmpty(message = "Run group must have a start address") String startAddress,
+                @Size(min = 5, max = 5, message = "Zipcode must be exactly 5 characters long")  String zipcode) {
+}
