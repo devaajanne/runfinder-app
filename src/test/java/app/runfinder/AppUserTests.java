f02c5e2fb4b01fa82d1185d6e4b61ac6999dd543
devaajanne <devaajanne@gmail.com>
2024-10-31 14:16:28 +0200

    Added unit tests

---
+++ src/test/java/app/runfinder/AppUserTests.java
@@ -0,0 +1,55 @@
+package app.runfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import app.runfinder.domain.entities.AppUser;
+import app.runfinder.domain.repositories.RoleRepository;
+import app.runfinder.domain.repositories.AppUserRepository;
+
+@DataJpaTest
+public class AppUserTests {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Test
+    public void addNewAppUserTest() {
+        long appUserCount = appUserRepository.count();
+
+        appUserRepository.save(
+                new AppUser(
+                        "testUser",
+                        "userFirstname",
+                        "userLastname",
+                        "user@runfinder.com",
+                        roleRepository.findByRole("USER"),
+                        "$2a$10$1KmOKZbxn09.ptn75m9ttOWn6X9YDkZZyQOwCURb3wgM6kwEnIcPy"));
+
+        assertThat(appUserRepository.count()).isEqualTo(appUserCount + 1);
+    }
+
+    @Test
+    public void findAppUserByIdTest() {
+        appUserRepository.save(
+                new AppUser(
+                        "testUser",
+                        "userFirstname",
+                        "userLastname",
+                        "user@runfinder.com",
+                        roleRepository.findByRole("USER"),
+                        "$2a$10$1KmOKZbxn09.ptn75m9ttOWn6X9YDkZZyQOwCURb3wgM6kwEnIcPy"));
+
+        Optional<AppUser> optionalAppUser = appUserRepository.findById(1L);
+        AppUser appUser = optionalAppUser.get();
+
+        assertThat(appUser.getAppUserId()).isEqualTo(1L);
+    }
+}
---
+++ src/test/java/app/runfinder/RunGroupTests.java
@@ -0,0 +1,101 @@
+package app.runfinder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import app.runfinder.domain.entities.RunGroup;
+import app.runfinder.domain.repositories.AppUserRepository;
+import app.runfinder.domain.repositories.ZipcodeRepository;
+import app.runfinder.domain.repositories.RunGroupRepository;
+
+@DataJpaTest
+public class RunGroupTests {
+
+    @Autowired
+    private RunGroupRepository runGroupRepository;
+
+    @Autowired
+    private ZipcodeRepository zipcodeRepository;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Test
+    public void addNewRunGroupTest() {
+        long runGroupCount = runGroupRepository.count();
+
+        runGroupRepository.save(new RunGroup(
+                "runGroup",
+                LocalDate.of(2024, 12, 30),
+                LocalTime.of(12, 0, 0),
+                "Juoksutie 2",
+                zipcodeRepository.findByZipcode("00100"),
+                null,
+                appUserRepository.findById(1L).get()));
+
+        assertThat(runGroupRepository.count()).isEqualTo(runGroupCount + 1);
+    }
+
+    @Test
+    public void findRunGroupByIdTest() {
+        runGroupRepository.save(new RunGroup(
+                "runGroup",
+                LocalDate.of(2024, 12, 30),
+                LocalTime.of(12, 0, 0),
+                "Juoksutie 2",
+                zipcodeRepository.findByZipcode("00100"),
+                null,
+                appUserRepository.findById(1L).get()));
+
+        Optional<RunGroup> optionalRunGroup = runGroupRepository.findById(1L);
+        RunGroup runGroup = optionalRunGroup.get();
+
+        assertThat(runGroup.getRunGroupId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void deleteRunGroupTest() {
+        runGroupRepository.save(new RunGroup(
+                "runGroup",
+                LocalDate.of(2024, 12, 30),
+                LocalTime.of(12, 0, 0),
+                "Juoksutie 2",
+                zipcodeRepository.findByZipcode("00100"),
+                null,
+                appUserRepository.findById(1L).get()));
+
+        Optional<RunGroup> optionalRunGroup = runGroupRepository.findById(1L);
+        RunGroup runGroup = optionalRunGroup.get();
+
+        runGroup.delete();
+
+        assertThat(runGroup.getDeletedAt()).isNotNull();
+    }
+
+    @Test
+    public void restoreRunGroupTest() {
+        runGroupRepository.save(new RunGroup(
+                "runGroup",
+                LocalDate.of(2024, 12, 30),
+                LocalTime.of(12, 0, 0),
+                "Juoksutie 2",
+                zipcodeRepository.findByZipcode("00100"),
+                null,
+                appUserRepository.findById(1L).get()));
+
+        Optional<RunGroup> optionalRunGroup = runGroupRepository.findById(1L);
+        RunGroup runGroup = optionalRunGroup.get();
+
+        runGroup.delete();
+        runGroup.restore();
+
+        assertThat(runGroup.getDeletedAt()).isNull();
+    }
+
+}
