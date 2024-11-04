15fde9e144cc8a9cb6bf351df58f27eece12ac5e
devaajanne <devaajanne@gmail.com>
2024-11-04 12:06:33 +0200

    Added SQL query to RunGroupRepository

--- src/main/java/app/runfinder/domain/repositories/RunGroupRepository.java
+++ src/main/java/app/runfinder/domain/repositories/RunGroupRepository.java
@@ -1,9 +1,14 @@
 package app.runfinder.domain.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import app.runfinder.domain.entities.RunGroup;
 
 public interface RunGroupRepository extends CrudRepository<RunGroup, Long> {
-     // No additional CRUD methods created
+
+     @Query(value = "SELECT * FROM run_groups r WHERE LOWER(r.run_group_name) LIKE CONCAT('%', LOWER(:searchParameter), '%') OR r.zipcode IN (SELECT zipcode from zipcodes z WHERE LOWER(z.city) LIKE CONCAT('%', LOWER(:searchParameter), '%'))", nativeQuery = true)
+     List<RunGroup> findBySearchParameter(String searchParameter);
 }
